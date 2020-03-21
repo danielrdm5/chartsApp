5cbcd2e446e744648cd5fb2768369a03032df1c2
danielrdm5 <daniel_rdm@hotmail.com>
2020-03-20 17:00:26 -0700

    Final charts on angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,24 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { ChartsModule } from 'ng2-charts';
+import { LineChartComponent } from './components/line-chart/line-chart.component';
+import { BarChartComponent } from './components/bar-chart/bar-chart.component';
+import { DonaChartComponent } from './components/dona-chart/dona-chart.component';
+import { RadarChartComponent } from './components/radar-chart/radar-chart.component';
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LineChartComponent,
+    BarChartComponent,
+    DonaChartComponent,
+    RadarChartComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ChartsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/components/dona-chart/dona-chart.component.ts
@@ -0,0 +1,33 @@
+import { Component, OnInit } from '@angular/core';
+import { ChartType } from 'chart.js';
+import { MultiDataSet, Label } from 'ng2-charts';
+
+@Component({
+  selector: 'app-dona-chart',
+  templateUrl: './dona-chart.component.html',
+  styleUrls: ['./dona-chart.component.css']
+})
+export class DonaChartComponent implements OnInit {
+
+  public doughnutChartLabels: Label[] = ['Download Sales', 'In-Store Sales', 'Mail-Order Sales'];
+  public doughnutChartData: MultiDataSet = [
+    [350, 450, 100],
+    [50, 150, 120],
+    [250, 130, 70],
+  ];
+  public doughnutChartType: ChartType = 'doughnut';
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  public chartClicked({ event, active }: { event: MouseEvent, active: {}[] }): void {
+    console.log(event, active);
+  }
+
+  public chartHovered({ event, active }: { event: MouseEvent, active: {}[] }): void {
+    console.log(event, active);
+  }
+
+}
